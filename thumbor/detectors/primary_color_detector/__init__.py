ac776d743cdac619512b5063c58ffa0d44a70e0d
maximka777 <maksim.borovskij@yandex.ru>
2018-09-18 12:12:17 -0700

    Wrapped into catch calculating of avg color

--- thumbor/detectors/primary_color_detector/__init__.py
+++ thumbor/detectors/primary_color_detector/__init__.py
@@ -10,6 +10,8 @@ from thumbor.utils import logger
 
 class Detector(BaseDetector):
 
+    # such implementation will conflict with other detectors
+
     def detect(self, callback):
         self.context.modules.engine.resize(self.context.request.width, self.context.request.height)
 
@@ -18,38 +20,45 @@ class Detector(BaseDetector):
 
         step = len(color_type)
 
-        colors = [color_string[i:i+step] for i in range(0, len(color_string), step)]
+        primary_color_str = None
 
-        # colors_dict = {}
+        try:
+            colors = []
 
-        numb_colors = [[struct.unpack('@B', component)[0] for component in color] for color in colors]
+            colors = [color_string[i:i+step] for i in range(0, len(color_string), step)]
 
-        points_count = len(numb_colors)
+            # colors_dict = {}
 
-        numb_colors = zip(*numb_colors)
+            numb_colors = [[struct.unpack('@B', component)[0] for component in color] for color in colors]
 
-        numb_colors = [sum(list) for list in numb_colors]
+            points_count = len(numb_colors)
 
-        numb_colors = [str(int(math.ceil(a / points_count))) for a in numb_colors]
+            numb_colors = zip(*numb_colors)
 
-        print(numb_colors)
+            numb_colors = [sum(list) for list in numb_colors]
 
-        # for color in colors:
-        #     if not color in colors_dict:
-        #         colors_dict[color] = 0
-        #     colors_dict[color] += 1
+            numb_colors = [str(int(math.ceil(a / points_count))) for a in numb_colors]
 
-        # primary_color = None
-        # max_color_count = 0
+            print(numb_colors)
 
-        # for key, val in colors_dict.items():
-        #     if val > max_color_count:
-        #         max_color_count = val
-        #         primary_color = key
+            # for color in colors:
+            #     if not color in colors_dict:
+            #         colors_dict[color] = 0
+            #     colors_dict[color] += 1
 
-        # primary_color_str = color_type.lower() + '(' + (', '.join([str(struct.unpack('@B', a)[0]) for a in primary_color])) + ')'
+            # primary_color = None
+            # max_color_count = 0
 
-        primary_color_str = color_type.lower() + '(' + (', '.join(numb_colors)) + ')'
+            # for key, val in colors_dict.items():
+            #     if val > max_color_count:
+            #         max_color_count = val
+            #         primary_color = key
+
+            # primary_color_str = color_type.lower() + '(' + (', '.join([str(struct.unpack('@B', a)[0]) for a in primary_color])) + ')'
+
+            primary_color_str = color_type.lower() + '(' + (', '.join(numb_colors)) + ')'
+        except:
+            primary_color_str = 'rgb(255, 255, 255, 255)'
 
         self.context.request.focal_points.append(
             PrimaryColorPoint(0, 0, primary_color_str)
