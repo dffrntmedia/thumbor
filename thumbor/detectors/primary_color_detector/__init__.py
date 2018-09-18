d1c0d0f8df696ca6c4a80db98ba66ad215506705
maximka777 <maksim.borovskij@yandex.ru>
2018-09-18 18:17:11 +0300

    Calc average color of image

--- thumbor/detectors/primary_color_detector/__init__.py
+++ thumbor/detectors/primary_color_detector/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import struct
+import math
 
 from thumbor.detectors import BaseDetector
 from thumbor.point import PrimaryColorPoint
@@ -10,6 +11,8 @@ from thumbor.utils import logger
 class Detector(BaseDetector):
 
     def detect(self, callback):
+        self.context.modules.engine.resize(self.context.request.width, self.context.request.height)
+
         image_data = self.context.modules.engine.image_data_as_rgb()
         color_type, color_string = image_data
 
@@ -17,22 +20,36 @@ class Detector(BaseDetector):
 
         colors = [color_string[i:i+step] for i in range(0, len(color_string), step)]
 
-        colors_dict = {}
+        # colors_dict = {}
 
-        for color in colors:
-            if not color in colors_dict:
-                colors_dict[color] = 0
-            colors_dict[color] += 1
+        numb_colors = [[struct.unpack('@B', component)[0] for component in color] for color in colors]
 
-        primary_color = None
-        max_color_count = 0
+        points_count = len(numb_colors)
 
-        for key, val in colors_dict.items():
-            if val > max_color_count:
-                max_color_count = val
-                primary_color = key
+        numb_colors = zip(*numb_colors)
 
-        primary_color_str = color_type.lower() + '(' + (', '.join([str(struct.unpack('@B', a)[0]) for a in primary_color])) + ')'
+        numb_colors = [sum(list) for list in numb_colors]
+
+        numb_colors = [str(int(math.ceil(a / points_count))) for a in numb_colors]
+
+        print(numb_colors)
+
+        # for color in colors:
+        #     if not color in colors_dict:
+        #         colors_dict[color] = 0
+        #     colors_dict[color] += 1
+
+        # primary_color = None
+        # max_color_count = 0
+
+        # for key, val in colors_dict.items():
+        #     if val > max_color_count:
+        #         max_color_count = val
+        #         primary_color = key
+
+        # primary_color_str = color_type.lower() + '(' + (', '.join([str(struct.unpack('@B', a)[0]) for a in primary_color])) + ')'
+
+        primary_color_str = color_type.lower() + '(' + (', '.join(numb_colors)) + ')'
 
         self.context.request.focal_points.append(
             PrimaryColorPoint(0, 0, primary_color_str)
